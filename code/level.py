d7af5da37a781d1bdbbc1e0a53274ae8519499e4
Trevor McCoy <trevorinsd@gmail.com>
2025-11-03 22:00:14 -0500

    Add health,energy, experience, and weapon UI overlay

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
 
 class Level:
 	def __init__(self):
@@ -22,6 +23,9 @@ class Level:
 
 		# sprite setup
 		self.create_map()
+
+		# user interface
+		self.ui = UI()
 
 	def create_map(self):
 		layouts = {
@@ -60,11 +64,12 @@ class Level:
 			self.current_attack.kill()
 		self.current_attack = None
 
+
 	def run(self):
 		# update and draw the game
 		self.visible_sprites.custom_draw(self.player)
 		self.visible_sprites.update()
-		debug(self.player.status)
+		self.ui.display(self.player)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
